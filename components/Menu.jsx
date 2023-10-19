47feac75517cf8e77aeff05465e189981d6c624f
BoFo <brunofazio466@gmail.com>
2023-10-18 22:14:37 -0300

    boceto inicial del menu

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -1,13 +1,21 @@
 import { useState } from 'react'
 
 export default function Menu() {
-    const [count, setCount] = useState(0)
+    const [selectedItem, setSelectedItem] = useState(null)
+
+    const items = ['juego de animales', 'hielo movil', 'el pinguino', 'el oso']
   return (
     <div>
         <h1>Menú</h1>
-      <h2>Contador</h2>
-      <p>{count}</p>
-      <button onClick={() => setCount((count) => count + 1)}>Click me</button>
+        <h2>Historias</h2>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index} onClick={() => setSelectedItem(item)}>
+            {item}
+          </li>
+        ))}
+      </ul>
+      {selectedItem && <p>Historia Seleccionada: {selectedItem}</p>}
     </div>
   )
 }
